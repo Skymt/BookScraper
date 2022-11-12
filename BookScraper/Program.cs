d9b497816e20258c5421a741dd43854c0eee89e9
Fredrik Schiller <fredrik.schiller@gmail.com>
2022-11-13 00:50:44 +0100

    Saved a using by making another line longer...

--- BookScraper/Program.cs
+++ BookScraper/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Diagnostics;
-
-Console.WriteLine("Initializing...");
+﻿Console.WriteLine("Initializing...");
 BookScraper.Scraper.Destination = @"D:\Temp";
 int initialBatch = await BookScraper.Scraper.Init();
 
@@ -12,4 +10,4 @@ BookScraper.Scraper.Process(threads: 16);
 Console.WriteLine("Processing done! Open scraped site? (Y/N)");
 
 if(Console.ReadKey(true).Key == ConsoleKey.Y)
-    Process.Start("explorer.exe", Path.Combine(BookScraper.Scraper.Destination, "index.html"));
+    System.Diagnostics.Process.Start("explorer.exe", Path.Combine(BookScraper.Scraper.Destination, "index.html"));
