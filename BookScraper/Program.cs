69d8837c75462b1cedf9ee9814dc9b5d9ed92e70
Fredrik Schiller <fredrik.schiller@1337.tech>
2023-05-22 11:51:22 +0200

    Experiments with .AsSpan()

--- BookScraper/Program.cs
+++ BookScraper/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Initializing...");
+﻿Console.ResetColor();
+Console.WriteLine("Initializing...");
 BookScraper.Scraper.Destination = @"D:\Temp";
 int initialBatch = await BookScraper.Scraper.Init();
 
@@ -6,7 +7,7 @@ Console.ForegroundColor= ConsoleColor.Green;
 Console.WriteLine($"Index.html scraped, found {initialBatch} initial files to process. Press any key to start!");
 Console.CursorVisible = false; Console.ReadKey(true);
 
-BookScraper.Scraper.Process(threads: 16);
+BookScraper.Scraper.Run(threadCount: 16);
 Console.WriteLine("Processing done! Open scraped site? (Y/N)");
 
 if(Console.ReadKey(true).Key == ConsoleKey.Y)
--- BookScraper/Scraper.cs
+++ BookScraper/Scraper.cs
@@ -1,171 +1,206 @@
 ﻿using System.Collections.Concurrent;
+using System.Data;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace BookScraper
 {
     internal static class Scraper
     {
         public static string Destination = @"C:\Temp";
-        static readonly ConcurrentQueue<string> _workload = new();
-        static readonly ConcurrentDictionary<string, bool> _process = new();
-        static readonly HttpClient _client = new() { BaseAddress = new("http://books.toscrape.com/") };
-        static readonly string[] _textExtensions = new[] { ".html", ".css", ".js" };
-        static readonly string[] _fileExtension = new[] { ".jpg", ".ico", ".eot", ".woff", ".ttf", ".svg" };
 
-        /// <summary>
-        /// Scrape index.html and return the amount of "interesting" links.
-        /// </summary>
-        /// <remarks>
-        /// Calling this is optional. <see cref="Process(int)"/> will work regardless.
-        /// </remarks>
-        /// <returns>The initial amount of files that should be processed</returns>
+        static readonly ConcurrentQueue<string> _textQueue = new();
+        static readonly ConcurrentQueue<string> _fileQueue = new();
+        static readonly ConcurrentDictionary<string, string?> _remoteToLocalMap = new();
+        static readonly HttpClient _client = new() { BaseAddress = new("http://books.toscrape.com/") };
+
         public static async Task<int> Init()
         {
-            _workload.Enqueue("index.html");
-            await ProcessWorkItem(-1);
-            return _workload.Count;
+            _remoteToLocalMap["/index.html"] = Path.Combine(Destination, "index.html");
+            _textQueue.Enqueue("/index.html");
+            await Next(); return _textQueue.Count + _fileQueue.Count;
         }
 
-        /// <summary>
-        /// Start threaded processing of the workload.
-        /// </summary>
-        /// <remarks>
-        /// If <see cref="Init"/> has not been called, root index.html will be evaluated as first item!
-        /// </remarks>
-        /// <param name="threads">The amount of threads to do work.</param>
-        public static void Process(int threads = 1)
+        public static void Run(int threadCount = 1)
         {
-            if (!_workload.Any())
+            // Report progress to console window title (This is technically +1 in threads, but since it sleeps a lot I don't count it...)
+            using Timer _ = new(_ => Console.Title = $"Total: {_remoteToLocalMap.Count}, Remaining: {_textQueue.Count + _fileQueue.Count}", null, 50, 100);
+            // Sanity check of the thread count
+            if (threadCount < 1) throw new ArgumentOutOfRangeException(nameof(threadCount), "At least one thread is required to run.");
+            // Setup the workers
+            var threads = Enumerable.Range(1, threadCount).Select(async id =>
             {
-                _workload.Enqueue("index.html");
-                ProcessWorkItem().Wait();
-            }
-
-            int currentCompleted = 1;
-            using var _ = new Timer(_ =>
-            {
-                var newCurrentCompleted = _process.Where(kvp => kvp.Value).Count();
-                if (newCurrentCompleted != currentCompleted)
+                while (true)
                 {
-                    currentCompleted = newCurrentCompleted;
-                    Log($"Current workload: {_workload.Count:0000}, Completed: {currentCompleted}", ConsoleColor.Red);
-                }
-            }, null, 50, 1000);
-
-            Task.WaitAll(Enumerable.Range(1, threads).Select(worker).ToArray());
-            Console.ForegroundColor = ConsoleColor.White;
-            return;
-
-            static async Task worker(int threadId)
-            {
-                int retries = 3;
-                do
-                {
-                    if (await ProcessWorkItem(threadId))
-                        retries = 3;
-                    else
+                    while (await Next(id)) ;
+                    await Task.Delay(100);
+                    if (!await Next(id))
                     {
-                        Log($"No work found for thread {threadId}... retrying ({retries})", ConsoleColor.DarkGreen);
-                        await Task.Delay(500);
-                        retries--;
+                        Log($"Thread {id} exited.", ConsoleColor.Green);
+                        break;
                     }
-                } while (retries > 0);
-                Log($"Thread {threadId} exited due to lack of work.", ConsoleColor.Green);
-            }
+                }
+            });
+            // Wait for all threads to complete.
+            Task.WaitAll(threads.ToArray());
+            Console.ResetColor();
         }
-
-        static async Task<bool> ProcessWorkItem(int threadId = 0)
+        static async Task<bool> Next(int threadId = -1)
         {
-            if (_workload.TryDequeue(out var filePath))
+            if (_textQueue.TryDequeue(out var text))
             {
-                FileInfo localFile = new(Path.Combine(Destination, filePath));
-                localFile.Directory?.Create();
+                var content = await _client.GetStringAsync(text);
+                var localFile = _remoteToLocalMap[text]!;
 
-                if (_textExtensions.Contains(localFile.Extension)) await DownloadText(filePath, localFile.FullName);
-                else if (_fileExtension.Contains(localFile.Extension)) await DownloadFile(filePath, localFile.FullName);
-                Log($"Thread {threadId} processed {filePath}", ConsoleColor.White);
-                return _process[filePath] = true;
+                int newLinkCount = Scrape2(text, content);
+                // While percent-encoded query strings are "allowed" by the file system, I don't like it!
+                content = content.Replace("%3Fv=3.2.1", string.Empty); // Local file names are also cleaned with _stripQueryPattern regex
+
+                if (!File.Exists(localFile))
+                    await File.WriteAllTextAsync(localFile, content);
+                if (threadId > -1) Log($"Thread {threadId} read {text} and found {newLinkCount} new links.", ConsoleColor.Yellow);
+                return true;
+            }
+            else if (_fileQueue.TryDequeue(out var file))
+            {
+                var localFile = _remoteToLocalMap[file]!;
+                if (!File.Exists(localFile))
+                {
+                    var content = await _client.GetByteArrayAsync(file);
+                    await File.WriteAllBytesAsync(localFile, content);
+                }
+                if (threadId > -1) Log($"Thread {threadId} downloaded {file}.", ConsoleColor.DarkYellow);
+                return true;
             }
             return false;
         }
-        static async Task DownloadText(string filePath, string localFilePath)
+
+        static readonly Regex _linkPattern = new(@"^((..\/)+|(\/))?([a-z0-9_-]+\/)*([a-zA-Z0-9-.]+)\.([a-zA-Z0-9-]+)(%.*)?$", RegexOptions.Compiled);
+        static readonly Regex _stripQueryPattern = new(@"[^%]+", RegexOptions.Compiled);
+        static readonly char[] _quotationChars = new[] { '"', '\'' };
+        static int Scrape(string remoteLink, string linkContent)
         {
-            var content = await _client.GetStringAsync(filePath);
-            Scrape(filePath, content);
-            // Fix for fontawesome versioning
-            content = content.Replace("%3Fv=3.2.1", string.Empty);
-            await File.WriteAllTextAsync(localFilePath, content);
-        }
-        static async Task DownloadFile(string filePath, string localFilePath)
-        {
-            var content = await _client.GetByteArrayAsync(filePath);
-            // Fix for fontawesome versioning
-            localFilePath = localFilePath.Replace("%3Fv=3.2.1", string.Empty);
-            await File.WriteAllBytesAsync(localFilePath, content);
-        }
-        static void Scrape(string filePath, string fileContent)
-        {
-            int newJobsCounter = 0;
-            foreach (var quotedString in scanForQuotedStrings())
+            var basePath = remoteLink.Split('/')[..^1];
+            if (basePath.Length == 0) basePath = new[] { string.Empty };
+
+            var quotedStrings = new List<string>();
+            var buffer = new StringBuilder(); char? quoteChar = null;
+            foreach (var c in linkContent.AsSpan())
             {
-                if (isValidFile(quotedString))
+                // Start reading the string by remembering the char that opened it
+                if (quoteChar == null && _quotationChars.Contains(c))
+                    quoteChar = c;
+                // Read while we know what char started the string
+                else if (quoteChar != null && quoteChar != c)
+                    buffer.Append(c);
+                // Until we get the same char again
+                else if (quoteChar == c)
                 {
-                    var mergedPath = mergePath(quotedString);
-                    if (!_process.ContainsKey(mergedPath) && _process.TryAdd(mergedPath, false))
-                    {
-                        _workload.Enqueue(mergedPath);
-                        newJobsCounter++;
-                    }
+                    quotedStrings.Add(buffer.ToString());
+                    buffer.Clear(); quoteChar = null;
                 }
             }
-            if (newJobsCounter > 0) Log($"Added {newJobsCounter} file(s) to process!", ConsoleColor.Yellow);
-            return;
 
-            IEnumerable<string> scanForQuotedStrings()
+            var counter = 0;
+            foreach (var file in quotedStrings.Where(stringIsALinkToFile).Select(theAbsoluteFilePath))
             {
-                var buffer = string.Empty; var reading = false;
-                var scanChar = '"';
-                if (filePath.EndsWith(".css"))
-                    scanChar = '\'';
-                foreach (var c in fileContent)
+                if (_remoteToLocalMap.TryAdd(file, null))
                 {
-                    if (c == scanChar)
+                    FileInfo localFile = new(Path.Combine(Destination, _stripQueryPattern.Match(file[1..]).Value));
+                    localFile.Directory?.Create();
+                    _remoteToLocalMap[file] = localFile.FullName;
+
+                    switch (localFile.Extension)
+                    {  // note: percent encoded query is stripped by the _stripQueryPattern regex
+                        case ".html": case ".css": case ".js":
+                            _textQueue.Enqueue(file); break;
+                        case ".jpg": case ".ico": case ".eot":
+                        case ".woff": case ".ttf": case ".svg":
+                            _fileQueue.Enqueue(file); break;
+                    }
+
+                    counter++;
+                }
+            }
+            return counter;
+            bool stringIsALinkToFile(string s) => _linkPattern.IsMatch(s);
+            string theAbsoluteFilePath(string path)
+            {
+                // path may already be absolute
+                if (path.StartsWith('/')) return path;
+
+                var relativeParts = path.Split('/');
+                var parents = relativeParts.TakeWhile(p => p == "..").Count();
+
+                // Range operator is really convenient! :D
+                return string.Join('/', basePath[..^parents].Concat(relativeParts[parents..]));
+            }
+        }
+        static int Scrape2(string remoteLink, string linkContent)
+        {
+            var basePath = remoteLink.Split('/')[..^1];
+            if (basePath.Length == 0) basePath = new[] { string.Empty };
+
+            var content = linkContent.AsSpan();
+            var ranges = Test(content); var counter = 0;
+            foreach(var range in ranges) 
+            {
+                var quotedString = content[range].ToString();
+                if(_linkPattern.IsMatch(quotedString))
+                {
+                    var file = absoluteFilePath(quotedString);
+                    if (_remoteToLocalMap.TryAdd(file, null))
                     {
-                        if (reading)
-                        {
-                            yield return buffer;
-                            buffer = string.Empty;
+                        FileInfo localFile = new(Path.Combine(Destination, _stripQueryPattern.Match(file[1..]).Value));
+                        localFile.Directory?.Create();
+                        _remoteToLocalMap[file] = localFile.FullName;
+
+                        switch (localFile.Extension)
+                        {  // note: percent encoded query is stripped by the _stripQueryPattern regex
+                            case ".html": case ".css": case ".js":
+                                _textQueue.Enqueue(file); break;
+                            case ".jpg": case ".ico": case ".eot":
+                            case ".woff": case ".ttf": case ".svg":
+                                _fileQueue.Enqueue(file); break;
                         }
-                        reading = !reading;
+
+                        counter++;
                     }
-                    else if (reading) buffer += c;
                 }
             }
-            static bool isValidFile(string s)
+            return counter;
+
+            string absoluteFilePath(string path)
             {
-                if (s.Contains("//")) return false;
-                if (s.Contains(' ')) return false;
-                if (!s.Contains('.')) return false;
+                // path may already be absolute
+                if (path.StartsWith('/')) return path;
 
-                // Fix for fontawesome versioning
-                if (s.EndsWith("%3Fv=3.2.1")) return true;
+                var relativeParts = path.Split('/');
+                var parents = relativeParts.TakeWhile(p => p == "..").Count();
 
-                var extension = s[s.LastIndexOf('.')..];
-                if (_textExtensions.Contains(extension)) return true;
-                if (_fileExtension.Contains(extension)) return true;
-                return false;
+                // Range operator is really convenient! :D
+                return string.Join('/', basePath[..^parents].Concat(relativeParts[parents..]));
             }
-            string mergePath(string s)
+        }
+        static Range[] Test(ReadOnlySpan<char> content)
+        {
+            var ranges = new List<Range>();
+            char? quoteChar = null; var (index, start) = (0, 0);
+            foreach (var c in content)
             {
-                if (s.StartsWith('/')) return s;
-                if (!filePath.Contains('/')) return s;
-
-                var basePath = filePath.Split('/')[..^1];
-                var relativePath = s.Split('/');
-                var parents = relativePath.TakeWhile(p => p == "..").Count();
-
-                var newPath = basePath[..^parents].Concat(relativePath[parents..]);
-                return string.Join('/', newPath);
+                if (quoteChar == null && _quotationChars.Contains(c))
+                {
+                    quoteChar = c;
+                    start = index + 1;
+                }
+                else if(quoteChar == c)
+                {
+                    ranges.Add(start..index);
+                    quoteChar = null;
+                }
+                index++;
             }
+            return ranges.ToArray();
         }
 
         static readonly object _consoleLock = new();
